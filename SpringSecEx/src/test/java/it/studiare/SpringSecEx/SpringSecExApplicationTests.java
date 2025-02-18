6880fc553728dc6b6ce6e4a0bcbafc6281f9f251
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-02-18 17:21:52 +0100

    ES TUTORIAL PER CAPIRE

---
+++ SpringSecEx/src/test/java/it/studiare/SpringSecEx/SpringSecExApplicationTests.java
@@ -0,0 +1,13 @@
+package it.studiare.SpringSecEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
