4703330a3c2bdae4f5924cd70d45b72f3f2b9b87
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-02-19 15:29:10 +0100

    some changes

--- SpringSecEx/src/main/java/it/studiare/SpringSecEx/StudentController.java
+++ SpringSecEx/src/main/java/it/studiare/SpringSecEx/StudentController.java
@@ -1,5 +1,7 @@
 package it.studiare.SpringSecEx;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,12 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+
     }
 
     @PostMapping("/students")
