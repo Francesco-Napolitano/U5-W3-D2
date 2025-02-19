79340d437e9f60e71896b3932077f2f9a5fb6cdf
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-02-19 13:04:35 +0100

    studio / ripasso

---
+++ SpringSecEx/src/main/java/it/studiare/SpringSecEx/StudentController.java
@@ -0,0 +1,26 @@
+package it.studiare.SpringSecEx;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private List <Student> students = new ArrayList<>(List.of(new Student(1, "Paolo", 75), new Student(2, "Francesco", 88), new Student(3, "Luca", 77)));
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student) {
+        students.add(student);
+        return student;
+    }
+}
