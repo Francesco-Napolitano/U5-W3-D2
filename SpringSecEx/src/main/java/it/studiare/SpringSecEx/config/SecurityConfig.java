4a5f257d0b4136271beaffe9f5feb09c5f0f9bac
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-02-24 15:44:31 +0100

    problems

--- SpringSecEx/src/main/java/it/studiare/SpringSecEx/config/SecurityConfig.java
+++ SpringSecEx/src/main/java/it/studiare/SpringSecEx/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package it.studiare.SpringSecEx.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -29,7 +31,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("register", "login")
+                        .permitAll()
+                        .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -73,5 +78,9 @@ public class SecurityConfig {
         return provider;
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
 
+    }
 }
--- SpringSecEx/src/main/java/it/studiare/SpringSecEx/service/UserService.java
+++ SpringSecEx/src/main/java/it/studiare/SpringSecEx/service/UserService.java
@@ -3,6 +3,9 @@ package it.studiare.SpringSecEx.service;
 import it.studiare.SpringSecEx.model.Users;
 import it.studiare.SpringSecEx.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +15,22 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    AuthenticationManager authManager;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public Users register(Users user) {
         user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication =
+                authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+    if (authentication.isAuthenticated())
+        return "success";
+
+    return "Fail";
     }
 }
