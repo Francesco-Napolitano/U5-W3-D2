56a93fa0f6ad350e9907de619df41f5eebb9082c
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-02-25 12:27:30 +0100

    validated JWT token

--- SpringSecEx/src/main/java/it/studiare/SpringSecEx/config/SecurityConfig.java
+++ SpringSecEx/src/main/java/it/studiare/SpringSecEx/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +28,9 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -37,6 +41,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
 //        Customizer<CsrfConfigurer <HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
