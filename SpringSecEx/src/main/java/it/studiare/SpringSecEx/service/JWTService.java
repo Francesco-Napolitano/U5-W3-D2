035e3962773a78fe5255dea5178b79ab2627397c
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-02-25 11:08:42 +0100

    generating different JWT

--- SpringSecEx/src/main/java/it/studiare/SpringSecEx/service/JWTService.java
+++ SpringSecEx/src/main/java/it/studiare/SpringSecEx/service/JWTService.java
@@ -5,7 +5,11 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +17,13 @@ import java.util.Map;
 @Service
 public class JWTService {
     private String secretKey = "sjSUdos8LSd";
+
+    public JWTService() throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+        SecretKey sk = keyGen.generateKey();
+        secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+    }
+
     public String generateToken(String username) {
 
 
@@ -24,7 +35,7 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() * 60 * 60 * 30))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
                 .and()
                 .signWith(getKey())
                 .compact();
